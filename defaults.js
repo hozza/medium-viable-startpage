2eb52f70d0cf69b4c1ea09fe64b0ec81fc8c86a3
hozza <hozza@users.noreply.github.com>
2020-06-02 17:21:40 +0000

    moved master_map / settings to separate file

---
+++ defaults.js
@@ -0,0 +1,51 @@
+const MASTER_MAP = [
+	{
+		groupName: "Studies",
+		items: [
+			{
+				name: "Item A",
+				shortcutKey: "q",
+				url: "https://google.com/?q=q"
+			},
+			{
+				name: "Item B",
+				shortcutKey: "w",
+				url: "https://google.com/?q=w"
+			},
+			{ name: "Item C", shortcutKey: "e", url: "https://google.com/?q=e" }
+		]
+	},
+	{
+		groupName: "Work",
+		items: [
+			{
+				name: "Item D",
+				shortcutKey: "a",
+				url: "https://google.com/?q=a"
+			},
+			{
+				name: "Item E",
+				shortcutKey: "s",
+				url: "https://google.com/?q=s"
+			},
+			{ name: "Item F", shortcutKey: "d", url: "https://google.com/?q=d" }
+		]
+	},
+	{
+		groupName: "Personal",
+		items: [
+			{
+				name: "Item I",
+				shortcutKey: "z",
+				url: "https://google.com/?q=z"
+			},
+			{
+				name: "Item J",
+				shortcutKey: "x",
+				url: "https://google.com/?q=x"
+			},
+			{ name: "Item K", shortcutKey: "c", url: "https://google.com/?q=c" }
+		]
+	}
+];
+
--- script.js
+++ script.js
@@ -11,59 +11,7 @@ const SHORTCUT_STARTER = "tab";
 // Also change --SHORTCUT_TIMEOUT in styles.css if you change this option.
 const SHORTCUT_TIMEOUT = 1500;
 
-// The groups of links are generated from this object. Edit it to edit the page's contents.
-// shortcutKey must hold an all-lowercase single button. Theoretically should work with values like `esc` and `f1`,
-// but intended to be used with just regular latin letters.
-const MASTER_MAP = [
-	{
-		groupName: "Studies",
-		items: [
-			{
-				name: "Item A",
-				shortcutKey: "q",
-				url: "https://google.com/?q=q"
-			},
-			{
-				name: "Item B",
-				shortcutKey: "w",
-				url: "https://google.com/?q=w"
-			},
-			{ name: "Item C", shortcutKey: "e", url: "https://google.com/?q=e" }
-		]
-	},
-	{
-		groupName: "Work",
-		items: [
-			{
-				name: "Item D",
-				shortcutKey: "a",
-				url: "https://google.com/?q=a"
-			},
-			{
-				name: "Item E",
-				shortcutKey: "s",
-				url: "https://google.com/?q=s"
-			},
-			{ name: "Item F", shortcutKey: "d", url: "https://google.com/?q=d" }
-		]
-	},
-	{
-		groupName: "Personal",
-		items: [
-			{
-				name: "Item I",
-				shortcutKey: "z",
-				url: "https://google.com/?q=z"
-			},
-			{
-				name: "Item J",
-				shortcutKey: "x",
-				url: "https://google.com/?q=x"
-			},
-			{ name: "Item K", shortcutKey: "c", url: "https://google.com/?q=c" }
-		]
-	}
-];
+// MASTER_MAP, and other settings will be loaded separately  
 
 let $container = document.getElementById("content");
 let getUrl = {};
